36e90101d3c372846e47d4c9508166315643f0f7
Panzerschrek <panzerschrek@Panzer-PC.(none)>
2013-09-27 15:17:31 +0700

    Добавил renderer.h и renderer.cpp

---
+++ src/renderer.cpp
@@ -0,0 +1,53 @@
+#include "renderer.h"
+
+Renderer::Renderer( Level* l, Player* p ):
+level(l), player(p)
+{
+}
+
+void Renderer::Draw()
+{
+	static float ang= 0.0f;
+
+    ang+=5.0f;
+
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -3.0f);
+    glRotatef( ang, 0.0f, 1.0f, 0.0f );
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* front face */
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.7f, 0.1f);  /* green */
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+
+    /* back face */
+    glColor3f(0.9, 1.0f, 0.0f);  /* yellow */
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    /* top side face */
+    glColor3f(0.2, 0.2, 1.0f);  /* blue */
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+
+    /* bottom side face */
+    glColor3f(0.7, 0.0f, 0.1);  /* red */
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glEnd();
+
+    glClearColor( 1.0f, 0.0f, 1.0f, 1.0f );
+}
---
+++ src/renderer.h
@@ -0,0 +1,37 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+
+#include "player.h"
+#include "level.h"
+
+#include <GL/gl.h>
+
+#ifdef MRPG_OS_WIN32
+#include "GL/wglext.h"
+#else
+//#include "GL/glxext.h"
+#include <GL/glx.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+#endif
+
+#include "GL/glext.h"
+
+
+class Renderer
+{
+	public:
+	Renderer( Level* l, Player* p );
+	~Renderer(){}
+
+	void Draw();
+
+	private:
+	Player* player;
+	Level* level;
+
+
+};
+
+#endif//RENDERER_H
